1293b833b78909360092b43682a93156bce498c6
Anusha Chavakula <achavakula@rythmos.com>
2020-12-21 17:58:57 +0530

    Add project files.

---
+++ NamespacesDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Namespace
+{
+    class Program
+    {
+        public void Pattern()
+        {
+            Console.WriteLine("In  First Namepace");
+            for(int i=0;i<=5;i++)
+            {
+                for(int j=0;j<=i;j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+}
+
+namespace NamespacesDemo
+{
+    class Program
+    {
+        public void Pattern()
+        {
+            Console.WriteLine("In Second Namespace");
+            for(int i=5;i>0;i--)
+            {
+                for(int j=0;j<i;j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Namespace.Program p = new Namespace.Program();
+            NamespacesDemo.Program p1 = new NamespacesDemo.Program();
+            p.Pattern();
+            p1.Pattern();
+        }
+    }
+}
